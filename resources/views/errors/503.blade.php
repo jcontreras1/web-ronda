0c0be27974f9c358b8cb1df127b1c7c34826e940
Jonathan Contreras <jcontreras@puerto.appm.com.ar>
2022-09-22 11:59:14 -0300

    Errores con imagenes

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -1,5 +1,9 @@
-@extends('errors::minimal')
+@extends('errors::illustrated-layout')
 
 @section('title', __('Service Unavailable'))
+@section('image')
+<img src="{{asset('assets/img/errors/503.jpg')}}" style="max-width: 100%;">
+@endsection
 @section('code', '503')
 @section('message', __('Service Unavailable'))
+
